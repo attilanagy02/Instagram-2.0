c358f864d749b746101f50b980872f6ef2e9c6e0
unknown <nagyatti2002@gmail.com>
2020-10-13 12:52:54 +0200

    Kész a teljes weblap

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
-function init() {
-    let img_holder = document.getElementById("img_holder");
-    for (let i = 0; i < data.length; i++) {
-        img_holder.innerHTML += 
-        '<div class="col-sm-12 col-md-6 col-lg-4">' +
-            '<div class="image" id="kep'+i+'"></div>' +
-            '</div>';
-    }
-}
-
 function getData(){
-    $.getJSON("http://cyrio.hu/mechwartgram/mechwartgram_data.json",function (json){
+    $.getJSON("data.json",function (json){
         let data = json.data;
+        console.log(data);
+         let img_holder = document.getElementById("img_holder");
+         for (let i = 0; i < data.length; i++) {
+             let id = "kep" + i;
+            img_holder.innerHTML += 
+            '<div class="col-sm-12 col-md-6 col-lg-4">' +
+                '<div class="image" id="'+id+'"></div>' +
+            '</div>';
+
+            let pic = document.getElementById(id);
+            pic.style.backgroundImage = 'url("'+data[i].pic+'")';
+         }
     })
 }
-init();
 getData();
